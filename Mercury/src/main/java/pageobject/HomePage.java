a27ccf90d1781ad39ebf83f94abc83d5d140c575
aditya-Home <adityasekhar03@gmail.com>
2014-03-16 23:18:57 +0530

    Added DriverFactory Class

--- Mercury/src/main/java/pageobject/HomePage.java
+++ Mercury/src/main/java/pageobject/HomePage.java
@@ -2,14 +2,18 @@ package pageobject;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class HomePage{
 	
+	private final Logger logger = LoggerFactory.getLogger(HomePage.class);
 	@FindBy(linkText="SIGN-ON")
 	private WebElement signOnLink;
 	
 	public void clickSignOn() {
 		signOnLink.click();
+		logger.trace("Click on WebElement : {} ",signOnLink);
 	}
 	
 
