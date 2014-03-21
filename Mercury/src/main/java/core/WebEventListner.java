e976a175d0a50269b37a0e1620c052005e05c6a6
aditya-Home <adityasekhar03@gmail.com>
2014-03-21 14:19:09 +0530

    Added proper log for listener events

--- Mercury/src/main/java/core/WebEventListner.java
+++ Mercury/src/main/java/core/WebEventListner.java
@@ -4,97 +4,87 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.events.WebDriverEventListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class WebEventListner implements WebDriverEventListener {
 
+	private final Logger log = LoggerFactory.getLogger("STEP");
 	@Override
 	public void beforeNavigateTo(String url, WebDriver driver) {
-		System.out.println("**********************  hi test");
+		log.info("START : Navigate To : {} ",url);
 
 	}
 
 	@Override
 	public void afterNavigateTo(String url, WebDriver driver) {
-		System.out.println("**********************  hi test");
-
+		log.info("FINISH : Navigate To : {} ",url);
 	}
 
 	@Override
 	public void beforeNavigateBack(WebDriver driver) {
-		System.out.println("**********************  hi test");
-
+		//TODO later implementation
 	}
 
 	@Override
 	public void afterNavigateBack(WebDriver driver) {
-		System.out.println("**********************  hi test");
 
 	}
 
 	@Override
 	public void beforeNavigateForward(WebDriver driver) {
-		System.out.println("**********************  hi test");
 
 	}
 
 	@Override
 	public void afterNavigateForward(WebDriver driver) {
-		System.out.println("**********************  hi test");
 
 	}
 
 	@Override
 	public void beforeFindBy(By by, WebElement element, WebDriver driver) {
-		System.out.println("**********************  hi test");
 
+		log.info("START : Operation On : {} ",by.toString());
 	}
 
 	@Override
 	public void afterFindBy(By by, WebElement element, WebDriver driver) {
-		System.out.println("**********************  hi test");
-
+		log.info("FINISH : Operation On : {} ",by.toString());
 	}
 
 	@Override
 	public void beforeClickOn(WebElement element, WebDriver driver) {
-		System.out.println(" *********************  HI Test");
-
+		log.info("START : Click On : {} ",element.getText());
 	}
 
 	@Override
 	public void afterClickOn(WebElement element, WebDriver driver) {
-		System.out.println("**********************  hi test");
-
+		log.info("FINISH : Click On : {} ",element.getText());
 	}
 
 	@Override
 	public void beforeChangeValueOf(WebElement element, WebDriver driver) {
-		System.out.println("**********************  hi test");
-
+		log.info("CURRENT : Element : {} , Value : {} ",element.getText(),element.getAttribute("value") );
 	}
 
 	@Override
 	public void afterChangeValueOf(WebElement element, WebDriver driver) {
-		System.out.println("**********************  hi test");
-
+		log.info("CHANGED : Element : {} , Value : {} ",element.getText(),element.getAttribute("value") );
 	}
 
 	@Override
 	public void beforeScript(String script, WebDriver driver) {
-		System.out.println("**********************  hi test");
-
+		log.info("START : Executing Script : {} ",script);
 	}
 
 	@Override
 	public void afterScript(String script, WebDriver driver) {
-		System.out.println("**********************  hi test");
-
+		log.info("FINISHED : Execution Script {}",script);
 	}
 
 	@Override
 	public void onException(Throwable throwable, WebDriver driver) {
-		System.out.println("**********************  hi test");
-
+		log.info("EXCEPTION : {} ",throwable.getMessage());
 	}
 
 }
